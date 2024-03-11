2e839740b3e48a37074e9733eab81358152c745e
AamirFromWitblox <aamir@witblox.com>
2024-03-11 18:26:47 +0530

    v0.2.0

--- bin/generate-app.js
+++ bin/generate-app.js
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 
 const { execSync } = require("child_process");
-const path = require("path");
 const fs = require("fs");
+const path = require("path");
 
 const args = process.argv.slice(2);
 if (args.length < 1) {
@@ -12,43 +12,57 @@ if (args.length < 1) {
 	process.exit(1); //an error occurred
 }
 
-console.log("Creating app with name:", args[0], " ...");
-
 const projectName = args[0];
 const currentPath = process.cwd();
-const projectPath = path.join(currentPath, projectName);
 const git_repo = `https://github.com/CodeWorxx/create-codeworx-app`;
 
 try {
-	fs.mkdirSync(projectPath);
-} catch (err) {
-	if (err.code === "EEXIST") {
-		console.log(
-			`The file ${projectName} already exist in the current directory, please give it another name.`
-		);
+	if (projectName !== ".") {
+		console.log("Creating app with name: ", projectName);
+		const isFolderExists = fs.existsSync(projectName);
+		if (isFolderExists) {
+			console.error(
+				`The file ${projectName} already exist in the current directory, please give it another name.`
+			);
+			process.exit(1);
+		}
 	} else {
-		console.log(error);
+		console.log("Creating app with name: ", path.basename(currentPath));
 	}
+} catch (err) {
+	console.log(error);
 	process.exit(1);
 }
 
 async function main() {
 	try {
 		console.log("Downloading files...");
+		// Git clone with depth 1 to only get the latest commit and add time out to prevent the process from running indefinitely
 		execSync(
-			`git clone --depth 1 --branch main-boilerplate ${git_repo} ${projectPath}`
+			`git clone --depth 1 --branch main-boilerplate ${git_repo} ${projectName}`,
+			{
+				encoding: "utf-8",
+				timeout: 10000,
+			}
 		);
 
-		process.chdir(projectPath);
+		process.chdir(projectName);
 
-		console.log("Installing dependencies...");
-		execSync("npm install");
+		// console.log("Installing dependencies...");
+		// execSync("npm install");
 
-		// console.log("Removing useless files");
-		// execSync("npx rimraf ./.git");
-		// fs.rmdirSync(path.join(projectPath, "bin"), { recursive: true });
+		// execSync("rm -rf .git");
+		// execSync("git init");
+		// execSync("git add .");
+		// execSync('git commit -m "Initial commit"');
 
-		console.log("The installation is done, this is ready to use !");
+		console.log("The app has been created successfully!");
+		console.log("\nTo get started, run the following commands:\n");
+		if (process.cwd() !== currentPath) {
+			console.log(`cd ${projectName}`);
+		}
+		console.log("npm install");
+		console.log("npm run dev");
 	} catch (error) {
 		console.log(error);
 	}
